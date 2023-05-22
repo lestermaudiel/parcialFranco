6f58dae14e906dd2907970aaf539b073610317ad
lestermaudiel <flestermaudiel2130@gmail.com>
2023-05-22 09:05:46 -0600

    Creacion clase cursos.

--- modelos/Cursos.php
+++ modelos/Cursos.php
@@ -3,3 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require 'Conexion.php';
+
+class Cursos extends Conexion{
+    public $curso_id;
+    public $curso_nombre;
+    public $curso_periodos;
+    public $curso_fase;
+    public $curso_instructor;
+    public $curso_creditos;
+}
