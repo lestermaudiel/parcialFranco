70af16983853e13721c273fbb1e85107b5d878c1
lestermaudiel <flestermaudiel2130@gmail.com>
2023-05-22 09:13:16 -0600

    Metodo Post

--- controladores/cursos/guardar.php
+++ controladores/cursos/guardar.php
@@ -3,3 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require '../../modelos/Cursos.php';
+try {
+    $cursos = new Cursos($_POST);
+    $resultado = $cursos->guardar();
+    $error = "No se guardó correctamente";
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2){
+    $error = $e2->getMessage();
+}
